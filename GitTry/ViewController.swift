4252c2f99165ed9d8ff323afc98dd4a4776a27cc
Sandeep Patharkar <DJSanddy@SanMac.local>
2017-10-20 11:53:08 -0400

    first commit

---
+++ GitTry/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTry
+//
+//  Created by Sandeep Patharkar on 10/20/17.
+//  Copyright © 2017 SFit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
